fa048cfd5d9d652c2cfa58c9e4c7a17bf9af7941
Peter Reynolds <97372919+pinewarbler@users.noreply.github.com>
2022-11-09 13:31:52 -0500

    added required console version of game

---
+++ src/ConsoleDriver.java
@@ -0,0 +1,15 @@
+public class ConsoleDriver {
+    public static void main(String[] args) {
+        // create console-based version of game (required by Dr. Hutchins)
+        // bulk of logic should be elsewhere. [REYNOLDS: should be similar to contents of Game.java]
+        // this should only have I/O logic
+        // meaning talk to user, get user input, show changes to user
+
+        // e.g.
+        // create an instance of type Game
+        // while loop allowed
+        // ask for user input
+        // call game.displayBooard() (of other similar method)
+
+    }
+}
