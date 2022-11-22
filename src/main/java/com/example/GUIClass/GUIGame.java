c3e772537d148a61794e8aedca7957baf2a4789d
Peter Reynolds <97372919+pinewarbler@users.noreply.github.com>
2022-11-22 14:14:18 -0500

    edited game application to have two stages--one for ship placement and another for ship hit/miss guessing

--- src/main/java/com/example/GUIClass/GUIGame.java
+++ src/main/java/com/example/GUIClass/GUIGame.java
@@ -5,27 +5,40 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GUIGame extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        new ShipPlacer();
     }
 }
+
+class ShipPlacer extends Stage{
+    Button openOther = new Button("Open game loop window");
+    HBox x = new HBox();
+
+    ShipPlacer(){
+        x.getChildren().add(openOther);
+        this.setScene(new Scene(x, 300, 300));
+        this.show();
+        openOther.setOnAction(t -> new GameLoopStage());
+    }
+}
+
+class GameLoopStage extends Stage {
+    Label x = new Label("Second stage");
+    VBox y = new VBox();
+
+    GameLoopStage(){
+        y.getChildren().add(x);
+        this.setScene(new Scene(y, 300, 300));
+        this.show();
+    }
+}
+
