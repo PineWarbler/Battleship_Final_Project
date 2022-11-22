7e7c5ac757aa6e7d5365b9543c4f247c545d431b
Peter Reynolds <97372919+pinewarbler@users.noreply.github.com>
2022-11-22 16:43:31 -0500

    added separate stages for different parts of the game; started to fill out the `settings` stage

--- src/main/java/com/example/GUIClass/GUIGame.java
+++ src/main/java/com/example/GUIClass/GUIGame.java
@@ -3,9 +3,12 @@ package com.example.GUIClass;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -15,19 +18,60 @@ import javafx.stage.Stage;
 public class GUIGame extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        new ShipPlacer();
+        new SettingsStage();
     }
 }
 
-class ShipPlacer extends Stage{
+class SettingsStage extends Stage {
+    VBox vb = new VBox();
+    Label difficultyLabel = new Label("Computer difficulty:");
+    RadioButton stoopitRadio = new RadioButton("stoopit");
+    RadioButton smartRadio = new RadioButton("smart");
+    RadioButton godRadio = new RadioButton("god-mode");
+    Button startGame = new Button("Start Game");
+    ToggleGroup tg = new ToggleGroup();
+
+    SettingsStage() {
+        smartRadio.setSelected(true); // default mode is `smart`
+        // adding the radio buttons to a toggle group makes only one button selectable at a time (what we want)
+        stoopitRadio.setToggleGroup(tg);
+        smartRadio.setToggleGroup(tg);
+        godRadio.setToggleGroup(tg);
+
+        vb.getChildren().addAll(difficultyLabel, stoopitRadio, smartRadio, godRadio, startGame);
+
+        startGame.setOnAction(new EventHandler<ActionEvent>() {
+            // this is for the startGame button
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String mode = "";
+                if (stoopitRadio.isSelected()) {
+                    mode = "stoopit";
+                    System.out.println("Stoopit was selected.");
+                } else if (smartRadio.isSelected()) {
+                    mode = "smart";
+                } else if (godRadio.isSelected()) {
+                    mode = "god";
+                }
+                this.hide();
+                new ShipPlacerStage(mode);
+            }
+        });
+        this.setScene(new Scene(vb, 500, 500));
+        this.show();
+    }
+}
+
+class ShipPlacerStage extends Stage{
     Button openOther = new Button("Open game loop window");
     HBox x = new HBox();
 
-    ShipPlacer(){
+    ShipPlacerStage(String mode){
+        // the `mode` parameter only here to be forwarded to the GameLoopStage
         x.getChildren().add(openOther);
         this.setScene(new Scene(x, 300, 300));
         this.show();
-        openOther.setOnAction(t -> new GameLoopStage());
+        openOther.setOnAction(t -> new GameLoopStage(mode));
     }
 }
 
@@ -35,10 +79,14 @@ class GameLoopStage extends Stage {
     Label x = new Label("Second stage");
     VBox y = new VBox();
 
-    GameLoopStage(){
+    GameLoopStage(String mode){
+        if(mode.equals("god")){
+            // play omnisciently
+        }
         y.getChildren().add(x);
         this.setScene(new Scene(y, 300, 300));
         this.show();
+
     }
 }
 
